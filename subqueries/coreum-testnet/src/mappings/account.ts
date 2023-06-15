fece4cd1f9d4b59c297904481fb9de69d7c5f56f
Metin Demir <metmirr@gmail.com>
2023-06-16 00:12:50 +0300

    feat: update coreum mappings to aggregate data

---
+++ subqueries/coreum-testnet/src/mappings/account.ts
@@ -0,0 +1,72 @@
+import { sendMessages } from '../common/kafka-producer'
+import { Account, AccountBalance, Contract } from '../types'
+import { ACCOUNT_BALANCE_TOPIC, BIGINT_ZERO, NEW_ACCOUNT_TOPIC } from '../common/constants'
+
+export async function getOrCreateAccount(accountId: string, chainId: string): Promise<Account> {
+  const account = await Account.get(accountId)
+
+  if (account) {
+    return account
+  }
+
+  const newAccount = Account.create({
+    id: accountId,
+    chainId,
+  })
+
+  sendMessages([newAccount], NEW_ACCOUNT_TOPIC)
+  return newAccount
+}
+
+export async function getOrCreateAccountBalance(
+  account: Account,
+  chainId: string,
+  contract?: Contract,
+): Promise<AccountBalance> {
+  const previousBalance = await AccountBalance.get(account.id)
+
+  if (previousBalance) {
+    return previousBalance
+  }
+
+  const newBalance = AccountBalance.create({
+    id: account.id,
+    accountId: account.id,
+    amount: BIGINT_ZERO,
+    blockNumber: 0,
+    timestamp: BIGINT_ZERO,
+    contractId: contract?.id,
+    chainId,
+  })
+  return newBalance
+}
+
+export async function decreaseAccountBalance(
+  account: Account,
+  amount: bigint,
+  chainId: string,
+  contract?: Contract,
+): Promise<AccountBalance> {
+  const balance = await getOrCreateAccountBalance(account, chainId, contract)
+  balance.amount -= amount
+  if (balance.amount < BIGINT_ZERO) {
+    balance.amount = BIGINT_ZERO
+  }
+  sendMessages([balance], ACCOUNT_BALANCE_TOPIC)
+
+  return balance
+}
+
+export async function increaseAccountBalance(
+  account: Account,
+  amount: bigint,
+  chainId: string,
+  contract?: Contract,
+): Promise<AccountBalance> {
+  const balance = await getOrCreateAccountBalance(account, chainId, contract)
+  balance.amount += amount
+
+  sendMessages([balance], ACCOUNT_BALANCE_TOPIC)
+
+  return balance
+}
--- subqueries/coreum-testnet/src/mappings/mappingHandlers.ts
+++ subqueries/coreum-testnet/src/mappings/mappingHandlers.ts
@@ -1,22 +1,61 @@
-import { TransferEvent, Message, Token, TokenTransfer, Transaction } from '../types'
-import { CosmosEvent, CosmosBlock, CosmosMessage, CosmosTransaction } from '@subql/types-cosmos'
+import { CosmosBlock, CosmosMessage, CosmosTransaction } from '@subql/types-cosmos'
 
-export async function handleBlock(block: CosmosBlock): Promise<void> {
-  const { time, height, chainId } = block.block.header
-  const { txs } = block
+import {
+  BIGINT_ZERO,
+  CHAIN_HOURLY_SNAPSHOT_TOPIC,
+  MILLISECONDS_PER_HOUR,
+  NEW_CONTRACT_TOPIC,
+} from '../common/constants'
+import { sendMessages } from '../common/kafka-producer'
+import { getTimestamp } from '../common/utils'
+import { ContractHourlySnapshot, Contract, ChainHourlySnapshot } from '../types'
+import { decreaseAccountBalance, getOrCreateAccount, increaseAccountBalance } from './account'
 
-  for (let i = 0; i < txs.length; i++) {
-    const { code, gasUsed } = txs[i]
+export async function handleTransaction(tx: CosmosTransaction): Promise<void> {
+  const hourlySnapshot = await getOrCreateChainHourlySnapshot(
+    tx.block.block.header.chainId,
+    tx.block,
+  )
+  hourlySnapshot.hourlyFailedTransactionCount += tx.tx.code === 0 ? 0 : 1
+  hourlySnapshot.hourlyGasConsumption += BigInt(tx.tx.gasUsed)
+  hourlySnapshot.hourlyTransactionCount += 1
 
-    const t = Transaction.create({
-      id: `${height}-${i}`,
-      blockHeight: height,
-      timestamp: BigInt(time.valueOf()),
-      chainId,
-      gasUsed: BigInt(gasUsed),
-      status: code === 0 ? 'success' : 'failed',
-    })
-    await t.save()
+  sendMessages([hourlySnapshot], CHAIN_HOURLY_SNAPSHOT_TOPIC)
+  await hourlySnapshot.save()
+}
+
+type TransferMsg = {
+  fromAddress: string
+  toAddress: string
+  amount: {
+    denom: string
+    amount: string
+  }[]
+}
+
+export async function handleTransferMessage(msg: CosmosMessage<TransferMsg>): Promise<void> {
+  const sender = msg.msg.decodedMsg.fromAddress
+  const recipient = msg.msg.decodedMsg.toAddress
+  const chainId = msg.block.header.chainId
+
+  for (const { denom, amount } of msg.msg.decodedMsg.amount) {
+    const senderAccountId = sender + '-' + denom
+    const senderAccount = await getOrCreateAccount(senderAccountId, chainId)
+    const senderBalance = await decreaseAccountBalance(senderAccount, BigInt(amount), chainId)
+    senderBalance.blockNumber = msg.block.header.height
+    senderBalance.timestamp = getTimestamp(msg.block)
+
+    await senderAccount.save()
+    await senderBalance.save()
+
+    const recipientAccountId = recipient + '-' + denom
+    const recipientAccount = await getOrCreateAccount(recipientAccountId, chainId)
+    const recipientBalance = await increaseAccountBalance(recipientAccount, BigInt(amount), chainId)
+    recipientBalance.blockNumber = msg.block.header.height
+    recipientBalance.timestamp = getTimestamp(msg.block)
+
+    await recipientAccount.save()
+    await recipientBalance.save()
   }
 }
 
@@ -33,69 +72,79 @@ type IssueMsg = {
 }
 
 export async function handleTokenIssuance(msg: CosmosMessage<IssueMsg>): Promise<void> {
-  const { subunit, features, issuer } = msg.msg.decodedMsg
-  const token = Token.create({
-    id: `${subunit}-${issuer}`,
-    ...msg.msg.decodedMsg,
-    features: JSON.stringify(features),
-    chainId: msg.block.header.chainId,
-    timestamp: BigInt(msg.block.block.header.time.valueOf()),
-  })
+  const { subunit, issuer, symbol } = msg.msg.decodedMsg
+
+  const tokenId = subunit + '-' + issuer
+  const token = await getOrCreateContract(tokenId, msg.block.header.chainId, symbol)
+
   await token.save()
 }
 
-/*
-export async function handleTransaction(tx: CosmosTransaction): Promise<void> {
-  // If you want to index each transaction in Cosmos (CosmosHub), you could do that here
-  const transactionRecord = Transaction.create({
-    id: tx.hash,
-    blockHeight: BigInt(tx.block.block.header.height),`
-    timestamp: tx.block.block.header.time,
-  });
-  await transactionRecord.save();
-}
-*/
+export async function getOrCreateContract(
+  address: string,
+  chainId: string,
+  name?: string,
+): Promise<Contract> {
+  const contract = await Contract.get(address)
+  if (contract) {
+    return contract
+  }
 
-type TransferMsg = {
-  fromAddress: string
-  toAddress: string
-  amount: {
-    denom: string
-    amount: string
-  }[]
+  const newContract = new Contract(address, chainId)
+  newContract.name = name
+
+  sendMessages([newContract], NEW_CONTRACT_TOPIC)
+
+  return newContract
 }
 
-export async function handleTransferMessage(msg: CosmosMessage<TransferMsg>): Promise<void> {
-  const { height, chainId, time } = msg.block.block.header
+async function getOrCreateContractHourlySnapshot(
+  contract: Contract,
+  block: CosmosBlock,
+): Promise<ContractHourlySnapshot> {
+  const snapshotId =
+    contract.id + '-' + Math.floor(block.header.time.valueOf() / MILLISECONDS_PER_HOUR).toString()
+  const previousSnapshot = await ContractHourlySnapshot.get(snapshotId)
 
-  const { amount } = msg.msg.decodedMsg
+  if (previousSnapshot) {
+    return previousSnapshot
+  }
 
-  const messageRecord = Message.create({
-    id: `${msg.tx.hash}-${msg.idx}`,
-    blockHeight: height,
-    chainId,
-    txHash: msg.tx.hash,
-    from: msg.msg.decodedMsg.fromAddress,
-    to: msg.msg.decodedMsg.toAddress,
-    amount: JSON.stringify(msg.msg.decodedMsg.amount),
+  const newSnapshot = ContractHourlySnapshot.create({
+    id: snapshotId,
+    contractId: contract.id,
+    hourlyTransactionCount: 0,
+    hourlyFailedTransactionCount: 0,
+    hourlyGasConsumption: BIGINT_ZERO,
+    blockNumber: BigInt(block.header.height),
+    timestamp: getTimestamp(block),
+    chainId: block.header.chainId,
   })
-  await messageRecord.save()
 
-  const tokens = []
-  for (const { denom } of amount) {
-    const token = await Token.get(denom)
-    if (token) {
-      tokens.push(token)
-    }
+  return newSnapshot
+}
+
+async function getOrCreateChainHourlySnapshot(
+  chainId: string,
+  block: CosmosBlock,
+): Promise<ChainHourlySnapshot> {
+  const snapshotId =
+    chainId + '-' + Math.floor(block.header.time.valueOf() / MILLISECONDS_PER_HOUR).toString()
+  const previousSnapshot = await ChainHourlySnapshot.get(snapshotId)
+
+  if (previousSnapshot) {
+    return previousSnapshot
   }
-  for (const token of tokens) {
-    const tokenTransfer = TokenTransfer.create({
-      ...messageRecord,
-      tokenId: token.id,
-      status: msg.tx.tx.code === 0 ? 'success' : 'failed',
-      gasUsed: BigInt(msg.tx.tx.gasUsed),
-      timestamp: BigInt(time.valueOf()),
-    })
-    await tokenTransfer.save()
-  }
+
+  const newSnapshot = ChainHourlySnapshot.create({
+    id: snapshotId,
+    hourlyFailedTransactionCount: 0,
+    hourlyGasConsumption: BIGINT_ZERO,
+    hourlyTransactionCount: 0,
+    blockNumber: BigInt(block.header.height),
+    timestamp: getTimestamp(block),
+    chainId: block.header.chainId,
+  })
+
+  return newSnapshot
 }
