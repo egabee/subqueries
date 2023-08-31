f589d5e5a4f2d496eb3c86e15e5e73fedf14df12
Metin Demir <metmirr@gmail.com>
2023-08-31 12:34:47 +0300

    feat: extract coreum types to a separete module

---
+++ subqueries/coreum-testnet/src/mappings/coreum-types.ts
@@ -0,0 +1,70 @@
+/**
+ * Represents a message for issuing an asset.
+ */
+export type IssueMsg = {
+  issuer: string
+  symbol: string
+  subunit: string
+  precision: number
+  initialAmount: string
+  description: string
+  features: string[]
+  burnRate: string
+  sendCommissionRate: string
+}
+
+/**
+ * Represents a coin with denomination and amount.
+ */
+export type Coin = {
+  denom: string
+  amount: string
+}
+
+/**
+ * Represents a message for minting new coins.
+ */
+export type MsgMint = {
+  sender: string
+  coin: Coin
+}
+
+/**
+ * Represents a message for burning coins.
+ */
+export type MsgBurn = {
+  sender: string
+  coin: Coin
+}
+
+/**
+ * Represents a message for freezing an account's coins.
+ */
+export type MsgFreeze = {
+  sender: string
+  account: string
+  coin: Coin
+}
+
+// MsgUnfreeze is the same as MsgFreeze
+export type MsgUnfreeze = MsgFreeze
+
+/**
+ * Represents a message for globally freezing a denomination.
+ */
+export type MsgGloballyFreeze = {
+  sender: string
+  denom: string
+}
+
+// MsgGloballyUnfreeze is the same as MsgGloballyFreeze
+export type MsgGloballyUnfreeze = MsgGloballyFreeze
+
+/**
+ * Represents a message for setting a whitelisted limit.
+ */
+export type MsgSetWhitelistedLimit = {
+  sender: string
+  account: string
+  coin: Coin
+}
