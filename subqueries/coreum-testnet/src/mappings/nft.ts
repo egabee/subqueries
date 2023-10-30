ecdabfd4ddf80418f17375eb68ac19b58cba228b
yahyamohmuedpro99 <133171914+yahyamohmuedpro99@users.noreply.github.com>
2023-10-30 12:07:36 +0200

    Create nft.ts (#6)
    * Create nft.ts
    
    add the nft mapping handler with this massages 
    -nft issue
    -mint
    -burn
    -freeze
    
    * Update nft.ts
    
    update the types of generic to be from the interface 
    rename the name of transactions of nft to not make conflict in kafka

---
+++ subqueries/coreum-testnet/src/mappings/nft.ts
@@ -0,0 +1,73 @@
+import { CosmosMessage } from '@subql/types-cosmos'
+import { sendBatchOfMessagesToKafka } from '../common/kafka-producer'
+import { TOPIC_MESSAGE } from '../common/constants'
+import { createTransaction } from './helper'
+
+export enum ClassFeature {
+  burning = 0,
+  freezing = 1,
+  whitelisting = 2,
+  disable_sending = 3,
+  UNRECOGNIZED = -1,
+}
+
+export interface MsgIssueClass {
+  issuer: string
+  symbol: string
+  name: string
+  description: string
+  uri: string
+  uriHash: string
+  data: any
+  features: ClassFeature[]
+  royaltyRate: string
+}
+
+export async function handleNftIssue(message: CosmosMessage<MsgIssueClass>): Promise<void> {
+  // to get any propertie of data 
+  // const issuer = message.msg.decodedMsg.issuer
+  // const symbol = message.msg.decodedMsg.symbol
+  const transaction = createTransaction('MsgIssueClass',message)
+  await sendBatchOfMessagesToKafka([{ messages: [transaction],topic: TOPIC_MESSAGE }])
+}
+
+export interface MsgMint {
+    sender: string;
+    classId: string;
+    id: string;
+    uri: string;
+    uriHash: string;
+    data: any;
+}
+export async function handleNftMsgMint(message:CosmosMessage<MsgMint>):Promise<void>{
+    // const mint=message.msg.decodedMsg.data
+
+    const transaction = createTransaction('MsgMintNFT',message)
+    await sendBatchOfMessagesToKafka([{messages:[transaction],topic:TOPIC_MESSAGE}])
+
+
+}
+
+export interface MsgBurn {
+  sender: string;
+  classId: string;
+  id: string;
+}
+export async function handleNftMsgBurn(message:CosmosMessage<MsgBurn>):Promise<void>{
+  // const burn=message.msg.decodedMsg.data
+  const transaction = createTransaction('MsgBurnNFT',message)
+  await sendBatchOfMessagesToKafka([{messages:[transaction],topic:TOPIC_MESSAGE}])
+    
+
+}
+
+export interface MsgFreeze {
+  sender: string;
+  classId: string;
+  id: string;
+}
+export async function handleNftMsgFreeze(message:CosmosMessage<MsgFreeze>):Promise<void>{
+  // const freeze=message.msg.decodedMsg.classId
+  const transaction=createTransaction('MsgFreezeNFT',message)
+  await sendBatchOfMessagesToKafka([{messages:[transaction],topic:TOPIC_MESSAGE}])
+}
