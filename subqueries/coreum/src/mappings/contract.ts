dffb6df3edda193224107698776848d8e33dc218
Metin Demir <metmirr@gmail.com>
2023-10-31 19:41:19 +0200

    fix: do not use generated message types from "types", they seem to be broken

--- subqueries/coreum/src/mappings/contract.ts
+++ subqueries/coreum/src/mappings/contract.ts
@@ -1,22 +1,19 @@
 import { CosmosMessage } from '@subql/types-cosmos'
 
-import {
-  MsgInstantiateContractMessage,
-  MsgExecuteContractMessage,
-} from '../types/CosmosMessageTypes'
+import { MsgInstantiateContract, MsgExecuteContract } from './coreum-types'
 import { createTransaction } from './helper'
 import { sendBatchOfMessagesToKafka } from '../common/kafka-producer'
 import { TOPIC_MESSAGE } from '../common/constants'
 
 export async function handleMsgInstantiateContract(
-  msg: CosmosMessage<MsgInstantiateContractMessage>,
+  msg: CosmosMessage<MsgInstantiateContract>,
 ): Promise<void> {
   const transaction = createTransaction('MsgInstantiateContract', msg)
   await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
 }
 
 export async function handleMsgExecuteContract(
-  msg: CosmosMessage<MsgExecuteContractMessage>,
+  msg: CosmosMessage<MsgExecuteContract>,
 ): Promise<void> {
   const transaction = createTransaction('MsgExecuteContract', msg)
   await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
